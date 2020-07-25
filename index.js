78a53e616ab5fb4748f55c9a127d43fc4a78d64d
daniyal214 <daniyal.hashmi2010@yahoo.com>
2020-07-26 04:53:20 +0500

    'Initial-commit'

---
+++ index.js
@@ -0,0 +1,75 @@
+var elem = document.querySelector('#boy');
+
+        elem.animate([
+            { transform: 'translate(-300px, 250px)' },
+            { transform: 'translate(1500px, 250px)' },
+            
+        ], {
+            duration: 10000,
+            iterations: Infinity,
+            playbackRate: -10,
+            
+        })
+
+        var elem = document.querySelector('#boy-green');
+
+        elem.animate([
+            { transform: 'scale(1) translate(0, 400px)' },
+            { transform: 'scale(1.8) translate(-1000px, 550px)' },
+            
+        ], {
+            duration: 8000,
+            iterations: Infinity,
+            playbackRate: -10,
+        })
+
+        var elem = document.querySelector('#plane');
+
+        elem.animate([
+            { transform: 'scale(1) translate(200px, 0)' },
+            { transform: 'scale(0.5) translate(2000px, -100px)' },
+            
+        ], {
+            duration: 20000,
+            iterations: Infinity,
+            playbackRate: -10,
+        })
+
+        var elem = document.querySelector('#birds');
+
+        elem.animate([
+            { transform: 'scale(1) translate(800px, 0)' },
+            { transform: 'scale(0.7) translate(-500px, -100px)' },
+            
+        ], {
+            duration: 9000,
+            iterations: Infinity,
+            playbackRate: -10,
+        })
+
+        var elem = document.querySelector('#van');
+
+        elem.animate([
+            { transform: 'translate(-500px, 350px)' },
+            { transform: 'translate(750px, 350px)' },
+            
+        ], {
+            duration: 8000,
+            iterations: Infinity,
+            playbackRate: -10,
+            
+        })
+
+
+        var elem = document.querySelector('#old');
+
+        elem.animate([
+            { transform: ' translate(500px, 500px)' },
+            { transform: ' translate(800px, 600px)' },
+            
+        ], {
+            duration: 10000,
+            iterations: Infinity,
+            playbackRate: -10,
+            
+        })
